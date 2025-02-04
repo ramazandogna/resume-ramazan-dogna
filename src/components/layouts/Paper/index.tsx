064d89423fb6c053606d853cca273abf66ee9901
Ramazan DOĞAN <doganrmzn40@gmail.com>
2025-02-04 18:07:30 +0300

    Components

---
+++ src/components/layouts/Paper/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ReactNode } from 'react';
+
+interface PaperProps {
+  children?: ReactNode;
+  pageNumber?: number;
+}
+
+const Paper: React.FC<PaperProps> = ({ children, pageNumber }) => {
+  return (
+    <div className="relative w-full max-w-[210mm] min-h-[240mm] p-[20mm] bg-white shadow-md border border-gray-300 mb-8 mx-auto">
+      {children}
+      {pageNumber !== undefined && (
+        <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-gray-500 text-sm">
+          {pageNumber}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Paper;
--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,3 +1,15 @@
+import Paper from '../components/layouts/Paper';
+
 export default function HomeLayout() {
-  return <div>Welcome to Home Page</div>;
+  return (
+    <div className="flex flex-col">
+      <Paper pageNumber={1}>
+        <div className="text-14px text-red-800 text-red-200">
+          Welcome to Home Page <br />
+          <button>ABC</button> <br />
+          <input type="text" placeholder="text." />
+        </div>
+      </Paper>
+    </div>
+  );
 }
